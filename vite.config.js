1cf1e108cdffee2c745de47b898944ea211936b4
local_work <local_work@laravel.ru>
2024-06-06 17:03:57 +0300

    W:06.06.2024

--- vite.config.js
+++ vite.config.js
@@ -12,5 +12,9 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  // server: {
+  //   host: 'ishop_front',
+  //   port: 9191
+  // }
 })
